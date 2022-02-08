ec4157973653ff9ed5659dc85593e508d23d9a85
theoverse <theoverse@protonmail.com>
2022-02-08 05:28:30 -0800

    added get weather api call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,24 @@
 const key = config.SECRET_API_KEY
 
+// get weather information
+const getWeather = async (id) => {
+    
+    // resource url 
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`
+
+    // query parameters
+    const response = await fetch(base + query)
+    const data = await response.json()
+
+    return data
+
+}
+
+// get city information
 const getCity = async (city) => {
 
-    // resource url
+    // resource url   
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
 
     // query parameters
@@ -16,6 +32,8 @@ const getCity = async (city) => {
 
 }
 
-getCity('miami')
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
+getCity('miami').then(data => {
+    return getWeather(data.Key)
+}).then(data => {
+    console.log(data)
+}).catch(err => console.log(err))
