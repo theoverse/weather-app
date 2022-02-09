05a58db05aa07df614af3f1569af69ee094cab79
theoverse <theoverse@protonmail.com>
2022-02-08 16:05:30 -0800

    modifieed forecast.js, added a foecast class

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,33 +1,25 @@
-const key = config.SECRET_API_KEY
+class Forecast {
+    constructor() {
+        this.key = config.SECRET_API_KEY
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/'
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    }
+    async updateCity(city) {
+        const cityDets = await this.getCity(city)
+        const weather = await this.getWeather(cityDets.Key)
+        return { cityDets, weather }
+    }
+    async getCity(city) {
+        const query = `?apikey=${this.key}&q=${city}`
 
-// get weather information
-const getWeather = async (id) => {
-    
-    // resource url 
-    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
-    const query = `${id}?apikey=${key}`
-
-    // query parameters
-    const response = await fetch(base + query)
-    const data = await response.json()
-
-    return data
-
-}
-
-// get city information
-const getCity = async (city) => {
-
-    // resource url   
-    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
-
-    // query parameters
-    const query = `?apikey=${key}&q=${city}`
-
-    const response = await fetch(base + query)
-    const data = await response.json()
-
-    // need Key field
-    return data[0]
-
+        const response = await fetch(this.cityURI + query)
+        const data = await response.json()
+        return data[0]
+    }
+    async getWeather(id) {
+        const query = `${id}?apikey=${this.key}`
+        const response = await fetch(this.weatherURI + query)
+        const data = await response.json()
+        return data
+    }
 }
